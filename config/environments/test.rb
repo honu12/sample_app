ae1fd36e604e5c254ad06465b8abc5723e2c7f9e
Honu <honu.menehune@gmail.com>
2016-03-04 01:21:20 +0000

    add password resets & email configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'https://ide.c9.io/honu12' }
+  config.action_mailer.default_url_options = { host: 'https://rails-tutorial-honu12.c9users.io' }
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
